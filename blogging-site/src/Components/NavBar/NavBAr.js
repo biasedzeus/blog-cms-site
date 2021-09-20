c0b05324046a1593f505b611e59b0f9d5819592a
biasedzeus <bhanu.singh1078@gmail.com>
2021-09-20 22:21:06 +0530

    NavBar Added

---
+++ blogging-site/src/Components/NavBar/NavBAr.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./NavBAr.css"
+
+
+
+
+function NavBar() {
+    return (
+        <div className="NavBar">
+            <ul className="links-container">
+    <li className="link-item"><a href="#" className="link">home</a></li>
+    <li className="link-item"><a href="#" className="link">general</a></li>
+    <li className="link-item"><a href="#" className="link">entertainment</a></li>
+    <li className="link-item"><a href="#" className="link">Technology</a></li>
+    <li className="link-item"><a href="#" className="link">Sports</a></li>
+    <li className="link-item"><a href="#" className="link">Health</a></li>
+    <li className="link-item"><a href="#" className="link">Science</a></li>
+
+
+</ul>
+        </div>
+    )
+}
+
+export default NavBar;
