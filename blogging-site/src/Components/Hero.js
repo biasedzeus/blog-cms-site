30773acec5221b36d4b1f66716e3008c4fb7e6c3
biasedzeus <bhanu.singh1078@gmail.com>
2021-09-20 22:20:21 +0530

    Hero Section

---
+++ blogging-site/src/Components/Hero.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Hero() {
+    return (
+        <div>
+            <header className="hero-section">
+    <div className="content">
+        {/* <img src="https://media.istockphoto.com/photos/neon-blog-sign-picture-id1270290167?b=1&k=20&m=1270290167&s=170667a&w=0&h=_5S-hkfJtimFnhM_9XOwu9HpVB2ya8vykNHi5e9BJeg=" className="logo" alt=""/> */}
+        <p className="sub-heading">Blogging is Not New, Here Its Different</p>
+    </div>
+</header>
+            
+        </div>
+    )
+}
+
+export default Hero
+
+
+
+
