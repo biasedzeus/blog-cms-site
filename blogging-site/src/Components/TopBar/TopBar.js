6f426e71986f74743a3aba6b0f08ee3e32430cf9
biasedzeus <bhanu.singh1078@gmail.com>
2021-09-21 22:02:21 +0530

    new flexbox TopBar

---
+++ blogging-site/src/Components/TopBar/TopBar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./TopBar.css"
+
+function TopBar() {
+    return (
+        <div className="top">
+            <div class="topbar-item">Home</div>
+            <div class="topbar-item">About</div>
+            <div class="topbar-item">Contact Us</div>
+            <div class="topbar-item">New Blog</div>
+            <div class="topbar-item">Notifications</div>
+            <div class="topbar-item">Login</div>
+
+            
+        </div>
+    )
+}
+
+export default TopBar
