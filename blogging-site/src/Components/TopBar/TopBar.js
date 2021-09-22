ae5c73b693c9bfc650e5635bbbeea6a0f71b0c82
biasedzeus <bhanu.singh1078@gmail.com>
2021-09-22 22:25:44 +0530

    Updates to TopBar

--- blogging-site/src/Components/TopBar/TopBar.js
+++ blogging-site/src/Components/TopBar/TopBar.js
@@ -23,6 +23,11 @@ function TopBar() {
             <div className="topRight">
             <div className="topbar-item">Notifications</div>
             <div className="topbar-item">Login</div>
+            <img
+              className="topImg"
+              src="https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80"
+              alt=""
+            />
             </div>
 
             
